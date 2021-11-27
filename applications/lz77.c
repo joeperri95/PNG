b7b4c560c93947b7bbc10eb4ad9a38db2f6a5040
joeperri95 <joeperri95@gmail.com>
2021-11-27 15:53:21 -0500

    Add lz77 implementation

--- applications/lz77.c
+++ applications/lz77.c
@@ -6,25 +6,44 @@
 #define WINDOW_SIZE 5
 #define SEARCH_SIZE 3
 
-void find_matches(uint8_t *buffer, uint32_t start_window, uint32_t start_search, uint8_t size)
-{
-	char *buff = malloc(size + 1);
-	buff[size] = '\0';
-	char *search_buff = malloc(size + 1);
-	search_buff[size] = '\0';
-	strncpy(search_buff, buffer + start_search, size);
+typedef struct _match{
+	uint32_t distance;
+	uint32_t length;
+	uint8_t literal;
+} match;
 
+match find_matches(uint8_t *buffer, uint32_t start_window, uint32_t start_search)
+{
+	int max_len = 0;
+	int distance = 0;
+	
 	for(int i = start_window; i < start_search; i++)
-{
-		strncpy(buff, buffer + i, size);
-		if(strcmp(buff, search_buff) == 0)
+	{
+		int j = 0;
+		while(buffer[i + j] == buffer[start_search + j])
 		{
-			LOG(glog, INFO, "Found a match at %d\n", i);
+			j++;
 		}	
-	}	
 
-	free(buff);
-	free(search_buff);
+		// match found	
+		if(j >= 3)
+		{
+		    if(j > max_len){
+				max_len = j;
+				distance = start_search - i;
+			}	
+		}	
+	}
+	if(max_len != 0)	
+		LOG(glog, INFO, "Found best match of length %d at distance %d\n", max_len, distance);
+
+	match result = {
+		.distance = distance,
+		.length = max_len,
+		.literal = buffer[start_search], 
+	}; 
+
+	return result;
 }
 
 
@@ -44,27 +63,35 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 
+	FILE *output = fopen(outfilename, "w");
 	uint8_t *file_buffer;
 	int64_t file_size = get_file_length(filename);
 	read_file_to_buffer(filename, &file_buffer);
 
 	uint32_t index = 0;
 	uint32_t window_start = 0;
-	uint8_t search_buffer[SEARCH_SIZE + 1];
-	uint8_t window_buffer[WINDOW_SIZE + 1];
-	
-	memset(window_buffer, 0, WINDOW_SIZE + 1);
-	memset(search_buffer, 0, SEARCH_SIZE + 1);
+	match next;	
 
-	while(index + SEARCH_SIZE < file_size){
-		index++;
+	while(index < file_size){
 		LOG(glog, DEBUG_3, "search buffer %.*s window buffer %.*s\n", SEARCH_SIZE, file_buffer + index, index - window_start, file_buffer + window_start);			
-		find_matches(file_buffer, window_start, index, SEARCH_SIZE);	
+		next = find_matches(file_buffer, window_start, index);	
+		if(next.length > 0)
+		{
+			index += next.length;
+			// just to illustrate the length, distance pair
+			fprintf(output, "<%d,%d>",next.distance, next.length);
+		}
+		else
+		{
+			index++;
+			fprintf(output, "%c",next.literal);
+		}		
+
 		if(index >= WINDOW_SIZE)
 		{
-			window_start++;
+			window_start = index - WINDOW_SIZE;
 		}
 	}
-
+	fclose(output);
 	return 0;
 }
